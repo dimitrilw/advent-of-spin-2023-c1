a2f27846c1fd84d3b148b00f66aaf3b21e5ab2de
Dimitri <5898931+dimitrilw@users.noreply.github.com>
2023-12-11 22:07:26 -0700

    implement solution; hurl tests pass
    Signed-off-by: Dimitri <5898931+dimitrilw@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,31 @@
-use spin_sdk::http::{IntoResponse, Request};
-use spin_sdk::http_component;
+use http::{Response, StatusCode};
+use spin_sdk::{
+    http::{IntoResponse, Method, Request},
+    http_component,
+    key_value::Store,
+};
 
-/// A simple Spin HTTP component.
+/// Fermyon advent-of-spin 2023, challenge 1
 #[http_component]
 fn handle_aos_chal_one(req: Request) -> anyhow::Result<impl IntoResponse> {
-    println!("Handling request to {:?}", req.header("spin-full-url"));
-    Ok(http::Response::builder()
-        .status(200)
-        .header("content-type", "text/plain")
-        .body("hello aos c1")?)
+    let store = Store::open_default()?;
+
+    Ok(match *req.method() {
+        Method::Get => match store.get(req.uri())? {
+            Some(value) => Response::builder()
+                .status(StatusCode::OK)
+                .header("content-type", "application/json")
+                .body(Some(value))?,
+            None => Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(None)?,
+        },
+        Method::Post => {
+            store.set(req.uri(), req.body())?;
+            Response::builder().status(StatusCode::CREATED).body(None)?
+        }
+        _ => Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(None)?,
+    })
 }
